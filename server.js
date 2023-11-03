f7df25e532ee627f6dcc9b743428a39c85341cf5
PhiHung1503 <phihung15032001@gmail.com>
2023-11-03 21:50:10 +0700

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connect } from 'mongoose';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// connect DB
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('API is running...');
@@ -15,5 +20,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running in http://locationhost/${PORT}`);
+    console.log(`Server running in http://localhost:${PORT}`);
 })
